c73c31e07a60a5eb988396f808a0b712d80cce81
egorulitin <egorulitin@gmail.com>
2018-07-17 23:57:12 +0300

    add func-component homework

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const MessageHistory = ({ list = [] }) => {
+  if(list.length === 0) return null;
+
+  return (
+    <ul>
+      {list.map(item => 
+        item.type === "response" ? (<Response from={item.from} message={item} />) : 
+        item.type === "message" ? (<Message from={item.from} message={item} />) :
+        item.type === "typing" ? (<Typing from={item.from} message={item} />) :
+        null
+      )}
+    </ul>
+  );
+};
--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,13 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
-}
+function Stars({ count }) {
+  const arr = new Array(count).fill([]);
+  
+  if(count < 1 || count > 5 || typeof count !== "number") return null;
+  
+  return ( 
+    <ul className="card-body-stars u-clearfix">
+      {arr.map(item => <li><Star /></li>)}
+    </ul>
+  );
+};
