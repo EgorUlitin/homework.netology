ee6604958b537eb7f847b2516c86c2c2136e423c
egorulitin <egorulitin@gmail.com>
2018-08-06 22:02:53 +0300

    add routing homework

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,16 @@
-
+const App = () => (
+  <Router>
+    <div className="tabs">
+      <nav className="tabs__items">
+        <NavLink exact className="tabs__item" to="/" activeClassName="tabs__item-active">Рефераты</NavLink>
+        <NavLink className="tabs__item" to="/creator" activeClassName="tabs__item-active">Криэйтор</NavLink>
+        <NavLink className="tabs__item" to="/fortune" activeClassName="tabs__item-active">Гадалка</NavLink>
+      </nav>
+      <div className="tabs__content">
+        <Route exact path="/" component={Essay} />
+        <Route exact path="/creator" component={Creator} />
+        <Route exact path="/fortune" component={Fortune} />
+      </div>
+    </div>
+  </Router>  
+)
