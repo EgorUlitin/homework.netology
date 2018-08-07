8124f563a81587adb742b4670f6c76e1809530a2
EgorUlitin <egorulitin@gmail.com>
2018-08-07 19:45:27 +0300

    fix

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -1,19 +1,41 @@
 class SearchBox extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = { fixed: false };
+    // this.isFixed = this.isFixed.bind(this);
+    this.myRef = null;
+
+    this.setInputRef = elem => {
+      this.myRef = elem;
+    };
+
+    this.scrollTop = () => {
+      this.myRef.focus();
+    };
   }
 
-  render() {
-    return <SearchBoxView fixed={this.state.fixed} />
+  componentDidMount() {
+    window.addEventListener('scroll', this.isFixed);
+    this.scrollTop();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.isFixed);
   }
 
   isFixed() {
-    return undefined;
+    const value = this.myRef;
+    console.log(value);
   }
 
   setPosition() {
     return undefined;
+  }
+
+  render() {
+    console.log(this.isFixed());
+    return (
+      <SearchBoxView fixed={this.state.fixed} ref={el => (this.myRef = el)} />
+    );
   }
 }
